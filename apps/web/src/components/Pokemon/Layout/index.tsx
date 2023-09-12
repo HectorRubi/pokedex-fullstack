bb30ba79f6cb6822bc020fbfe718fff05815b76f
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-09-12 17:33:24 -0600

    Add tabs

--- apps/web/src/components/Pokemon/Layout/index.tsx
+++ apps/web/src/components/Pokemon/Layout/index.tsx
@@ -1,5 +1,15 @@
+import { Tabs } from 'flowbite-react'
+import { HiStar } from 'react-icons/hi'
+import { MdDashboard } from 'react-icons/md'
 import { PokemonList } from './../List'
 
 export function PokemonLayout() {
-  return <PokemonList />
+  return (
+    <Tabs.Group aria-label="Default tabs" style="default">
+      <Tabs.Item active icon={MdDashboard} title="All">
+        <PokemonList />
+      </Tabs.Item>
+      <Tabs.Item icon={HiStar} title="Favorites"></Tabs.Item>
+    </Tabs.Group>
+  )
 }
