8f88c41e94c91111adfb200eaafcd5133e220735
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-09-13 11:42:39 -0600

    Create make file

---
+++ Makefile
@@ -0,0 +1,16 @@
+start:
+	make simple-start
+	chmod +x ./scripts/check-database-ready.sh
+	./scripts/check-database-ready.sh
+	make init
+
+simple-start:
+	docker-compose up -d
+
+stop:
+	docker-compose down
+	docker image rm pokedex_app
+	docker image rm pokedex_server
+
+init:
+	docker-compose exec server npm run migration:run
---
+++ scripts/check-database-ready.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+searching=1
+while [ $searching -eq 1 ]
+do
+  volume=$(docker volume ls  -q | grep -o "app-test-express-react_db_data")
+  res=$(docker-compose logs db | grep -o "database system is ready to accept connections" | wc -l)
+  if [ $res -eq 2 ] || [ $volume ]
+  then
+    searching=$(( $searching + 1))
+  fi
+done
+echo 'done'
